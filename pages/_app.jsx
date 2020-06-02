3661254113a4af2eeddcc586370d11f5dfe1fb0c
Andrew Ford <andrew@andrewford.co.nz>
2020-06-03 11:10:53 +1200

    Added intro and fleshing out page structure

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,12 +9,15 @@ export default function App(props) {
                 <GithubLink key='0' url='https://github.com/remorses/dokz' />,
                 <ColorModeSwitch key='1' />,
             ]}
-            sidebarOrdering={{
-                'index.mdx': true,
-                Documents_Group: {
-                    'another.mdx': true,
-                },
-            }}
+            headerLogo={
+                <img
+                    src='/learning-reactjs.svg'
+                    style={{ opacity: 0.8 }}
+                    width='300px'
+                />
+            }
+            initialColorMode='dark'
+            
         >
             <Component {...pageProps} />
         </DokzProvider>
